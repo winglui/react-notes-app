83987d09ab4a551a74d3b025142dd2a2fdeade4a
Wing Lui <winglui@gmail.com>
2026-02-12 16:16:52 -0500

    - added package.json to build and start both frontend and backend. - updated server.js to serve UI as well. - added env variables.

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,23 @@ import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from 'cors'
+import path from 'path'
 
 
 dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 //middleware
 app.use(express.json())
-app.use(cors({
-  origin: "http://localhost:5173"
-}))
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({
+    origin: "http://localhost:5173"
+  }))
+}
 app.use(rateLimiter)
 
 
@@ -25,6 +30,14 @@ app.use((req, res, next) => {
 })
 
 app.use("/api/notes", notesRoutes)
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
+  })
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
